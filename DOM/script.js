9677b4886c752c3f406f4d02440c58a2a9b4dc2e
dudaborges <duda.pborges92@gmail.com>
2022-11-21 18:44:44 -0300

    update

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,15 @@
 let h1 = document.querySelector('h1')
 h1.style.color = 'red'
+
+
+let button = document.getElementById('button')
+
+button.addEventListener('click', function(){
+    let teste = document.getElementById('teste')
+
+    if(teste.style.display === 'none'){
+        teste.style.display = 'block'
+    }else{
+        teste.style.display = 'none'
+    }
+})
